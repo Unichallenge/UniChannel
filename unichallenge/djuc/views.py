561870bc4ce5dfd1e0ed96b6ece4bad8a64de155
Theonimfi <theon.anog@gmail.com>
2019-05-03 20:02:47 +0300

    Add TagList in views

--- unichallenge/djuc/views.py
+++ unichallenge/djuc/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from .models import Tags
+from .serializers import TagSerializer
+from django.contrib.staticfiles import views
 
-def index(request):
-    return HttpResponse("Hello, world. You are at the djbr index.")
+from rest_framework import permissions
+from rest_framework import generics
+
+class TagsList(generics.ListCreateAPIView):
+    serializer_class =  TagSerializer
+
+    def get_queryset(self):
+        queryset = Tags.objects.all()
+        tag_name = self.request.query_params.get('tags', None)
+        if tag_name is not None:
+            queryset = queryset.filter(tags__contains=tag_name)
+        return queryset
