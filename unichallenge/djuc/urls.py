5993d8a5590eca23147955f6318cee280d7c3e00
claire <fr-claire@hotmail.com>
2019-05-04 14:11:49 +0300

    adding to api PiostLists more than one tags

--- unichallenge/djuc/urls.py
+++ unichallenge/djuc/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path('tags/', views.TagsList.as_view(), name='taglist')
     re_path(r'^tags/?$', views.TagsList.as_view()),
     # re_path(r'^posts/?$', views.PostList.as_view()),
-    re_path ( r'^posts/tags/(?P<tag>\w+)/?$', views.PostList.as_view () )
+    re_path ( r'^posts/tags/(?P<tag>[\w,]+)/?$', views.PostList.as_view () )
 ]
