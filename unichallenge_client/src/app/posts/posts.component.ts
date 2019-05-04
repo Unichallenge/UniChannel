f348c9ab57bd8430080f2b094e39ab65e2bce7a1
Theonimfi <theon.anog@gmail.com>
2019-05-04 15:18:06 +0300

    Create post services

--- unichallenge_client/src/app/posts/posts.component.ts
+++ unichallenge_client/src/app/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Post} from '../post';
 
+import { PostService } from '../post.service';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -10,7 +12,7 @@ export class PostsComponent implements OnInit {
 
   @Input() posts: Post[];
 
-  constructor() { }
+  constructor(private PostService: PostService) { }
 
   ngOnInit() {
   }
