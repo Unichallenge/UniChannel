87ec1207fe0c4eadca1795fdf2c6c334bc8b8e46
enderian <spyrospagk@gmail.com>
2019-05-04 17:15:38 +0300

    Small changes.

--- unichallenge/unichallenge/settings.py
+++ unichallenge/unichallenge/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2h4!tqc&-3lin2$)99s-2urahri$*#)uowz4(*zo@coc%(=@k2'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
