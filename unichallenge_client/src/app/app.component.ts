c5637af3761ab01f1832623539eaeae14e5ee23b
enderian <spyrospagk@gmail.com>
2019-05-04 14:11:10 +0300

    Oof - a lot of stuffz

--- unichallenge_client/src/app/app.component.ts
+++ unichallenge_client/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
     @ViewChild('contentStart') contentStart: ElementRef;
+
+    collapse: boolean;
+
+    ngOnInit(): void {
+
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    scrollHandler(event) {
+        this.collapse = this.contentStart.nativeElement.getBoundingClientRect().top < 90;
+    }
+
 
 }
